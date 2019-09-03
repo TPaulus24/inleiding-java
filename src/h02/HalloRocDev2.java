cce5d354edf896b74882f972cd9ea86a83ce32a4
TPaulus24 <54597104+TPaulus24@users.noreply.github.com>
2019-09-03 12:18:53 +0200

    H02

--- src/h02/HalloRocDev2.java
+++ src/h02/HalloRocDev2.java
@@ -3,14 +3,16 @@ package H02;
 import java.awt.*;
 import java.applet.*;
 
-public class HalloRocDev extends Applet {
+public class HalloRocDev2 extends Applet {
 
     public void init() {
-        setBackground(Color.blue);
+        setBackground(Color.white);
     }
 
     public void paint(Graphics g) {
-        g.setColor(Color.yellow);
-        g.drawString("Tom Paulus", 50, 60 );
+        g.setColor(Color.blue);
+        g.drawString("Tom", 55, 60 );
+        g.setColor(Color.red);
+        g.drawString("Paulus" , 50, 80);
     }
 }
