b33533b5f32c27f964d7e12425d70214037edf4c
TPaulus24 <54597104+TPaulus24@users.noreply.github.com>
2019-09-04 12:12:08 +0200

    2.1

---
+++ src/h04/HalloRocDev5.java
@@ -0,0 +1,19 @@
+package h04;
+
+import java.awt.*;
+import java.applet.*;
+
+public class HalloRocDev5 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.blue);
+        g.setColor(Color.yellow);
+        g.fillArc(100, 100, 100, 50, 90, 360);
+
+
+
+    }
+}
